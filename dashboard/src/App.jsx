6e0a5d5d895638aa9108a1dc1161686e68fc6168
Payaldas <payaldaskunu30@gmail.com>
2025-03-15 17:12:04 +0530

    dashboard

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import Dashboard from './Dashboard'
+
+function App() {
+  return (
+    <div className='bg-gray-900'>
+      <Dashboard/>
+    </div>
+  )
+}
+
+export default App
